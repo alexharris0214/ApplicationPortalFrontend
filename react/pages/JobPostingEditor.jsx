4e2202927ccaff64694f0efb7d4553b1c79a1bd1
jdeiss9 <jdeiss1001@gmail.com>
2024-08-26 14:33:39 -0400

    About to merge

--- react/pages/JobPostingEditor.jsx
+++ react/pages/JobPostingEditor.jsx
@@ -5,16 +5,52 @@ import PostModal from '../components/PostModal/PostModal';
 import { AuthContext } from '../context/AuthContext';
 import JobFeed from '../components/FeedCard/JobFeed';
 import ManagerJobFeed from '../components/FeedCard/ManagerJobFeed';
+import { useParams } from 'react-router-dom';
 
 const JobPostingEditor = () => {
-    const [jobs, setJobs] = useState([]);
+    const { jobId } = useParams();
+    console.log(jobId);
+    // const [jobs, setJobs] = useState([]);
 
-    useEffect(() => {
-        const fetchJobs = async
-    })
-    return <>
-    Hello!
-    </>
+    // useEffect(() => {
+    //     const fetchJobs = async
+    // })
+    function handleSubmit () {
+        return <h1>Hello</h1>
+    }
+    return <div className='div' style={styles.div}>
+        <h1>Edit Job Posting</h1>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Listing Title: 
+                <input type="text" />
+            </label>
+            
+            <label>
+                Job Title: 
+                <input type="text" value/>
+            </label>
+
+            <label>
+                Job Description: 
+                <input type="text" />
+            </label>
+
+            <label>
+                Availability Status: 
+                <input type="text" />
+            </label>
+            <button type="submit">Update & Save</button>
+        </form>
+    </div>
 }
+const styles = {
+    div: {
+        backgroundColor: 'white',
+        border: '1px solid black',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        padding: '16px',
+    },
+  };
 
 export default JobPostingEditor;
