7bc568d28978337ff4f1b7668b751cb43bc0d36f
jdeiss9 <jdeiss1001@gmail.com>
2024-08-26 16:00:12 -0400

    Started edit job modal (Jeff)

--- react/components/FeedCard/JobCard.jsx
+++ react/components/FeedCard/JobCard.jsx
@@ -4,15 +4,33 @@ import { AuthContext } from '../../context/AuthContext';
 
 const JobCard = ({ job }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const { user } = useContext(AuthContext);
 
   const handleApplyClick = () => {
     setIsModalOpen(true);
   };
 
+  const handleEditClick = () => {
+    setIsEditModalOpen(true);
+  }
+
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleDelete = () => {
+    console.log("Deleting job");
+  }
+
+  // const handleEdit = () => {
+  //   console.log(job.id);
+  //   navigate(`/manager/edit/${job.id}`);
+  // }
+
+  const viewApps = () => {
+    console.log("Viewing applications");
+  }
 
   return (
     <div className="card" style={styles.card}>
@@ -26,6 +44,14 @@ const JobCard = ({ job }) => {
       {job.jobDescription && <p><strong>Description:</strong> {job.jobDescription}</p>}
       {/* {job.selectedCandidateId && <p><strong>Selected Candidate ID:</strong> {job.selectedCandidateId}</p>} */}
 
+      {user && user.role === 'RECRUITER' && (
+        <div>
+        <button onClick={handleDelete} className="style-button">
+        <strong>Delete Job Posting</strong></button>
+        <button onClick={handleEditClick} className='style-button' ><strong>Edit Job Postings</strong></button>
+        <button onClick={viewApps} className='style-button'><strong>View Applications</strong></button>
+        </div>
+      )}
 
       {/* Conditionally render the Apply button */}
       {user && user.role==='CANDIDATE' && (
@@ -49,7 +75,14 @@ const styles = {
     border: '1px solid black',
     boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
     padding: '16px',
-  },
+  }
+  // ,
+  // button: {
+  //   backgroundColor: "white",
+  //   border: '1px solid black',
+  //   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  //   padding: '16px 32px'
+  // }
 };
 
 export default JobCard;
